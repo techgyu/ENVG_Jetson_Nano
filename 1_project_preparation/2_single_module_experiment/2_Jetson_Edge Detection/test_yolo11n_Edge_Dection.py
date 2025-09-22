9de674c6da6d83203ba9bcd70c13ce7f9efb28a5
macbook_windows <rlarbxor85@gamil.com>
2025-09-22 17:37:28 +0900

    디렉토리 경로 수정

--- 1_project_preparation/2_single_module_experiment/2_Jetson_Edge Detection/test_yolo11n_Edge_Dection.py
+++ 1_project_preparation/2_single_module_experiment/2_Jetson_Edge Detection/test_yolo11n_Edge_Dection.py
@@ -6,24 +6,27 @@ if not hasattr(np, 'bool'):
     np.bool = bool
 
 # 1. 모델 로드
-model = YOLO("yolo11n.pt")
+model = YOLO("./1_project_preparation/2_single_module_experiment/2_Jetson_Edge Detection/data/model/yolo11n.pt")
 
-# 2. 이미지 추론
-results = model("bus.jpg")
+# 2. 이미지 경로 변수로 지정
+img_path = "./1_project_preparation/2_single_module_experiment/2_Jetson_Edge Detection/data/image/bus.jpg"
 
-# 3. 탐지 결과 순회
-img = cv2.imread("bus.jpg")
+# 3. 이미지 추론
+results = model(img_path)
+
+# 4. 원본 이미지 읽기
+img = cv2.imread(img_path)
 
 for r in results[0].boxes.xyxy.cpu().numpy():
     x1, y1, x2, y2 = map(int, r[:4])
     roi = img[y1:y2, x1:x2]
 
-    # 4. 외곽선 추출
+    # 5. 외곽선 추출
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 100, 200)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 5. 원본에 그리기
+    # 6. 원본에 그리기
     cv2.drawContours(img[y1:y2, x1:x2], contours, -1, (0,255,0), 2)
 
-cv2.imwrite("bus_with_contours.jpg", img)
+cv2.imwrite("./1_project_preparation/2_single_module_experiment/2_Jetson_Edge Detection/data/image/bus_with_contours.jpg", img)
